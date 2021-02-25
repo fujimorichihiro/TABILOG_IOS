867fa725443d4f5712c501251e511648e2768706
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-25 14:39:40 +0900

    Initial Commit

---
+++ TabilogIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabilogIOS
+//
+//  Created by 藤森千裕 on 2021/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabilogIOS/TabilogIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabilogIOSApp.swift
+//  TabilogIOS
+//
+//  Created by 藤森千裕 on 2021/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TabilogIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
